311c7063d7954c9c0a82b73aeb17eb6591b7be10
dolpast <teroru720phi@gmail.com>
2025-09-22 04:43:14 +0900

    紆余曲折した

--- sb.py
+++ sb.py
@@ -7,7 +7,10 @@ import tkinter as tk
 import tkinter.filedialog
 import pandas as pd
 import json
+import datetime
+from datetime import timedelta
 
+# 現在の日付時刻を表す文字列を返す
 def get_now_datetime_txt():
     now = datetime.datetime.now()
     year = str(now.year).zfill(4)
@@ -18,6 +21,46 @@ def get_now_datetime_txt():
     second = str(now.second).zfill(2)
     datetime_txt = year + '-' + month + '-' + day + ' ' + hour + '-' + minute + '-' + second
     return datetime_txt
+
+
+def daterange(start, end):
+    for n in range((end - start).days + 1):
+        yield start + timedelta(n)
+
+
+def distribute_on_timeline(data):
+
+    dates = list()
+    for msg in data:
+        dates.append(datetime.datetime.strptime(msg["date"], "%Y/%m/%d %H:%M:%S").date())
+    minDate = min(dates)
+    print(f"minDate = {minDate}")
+    maxDate = max(dates)
+    print(f"maxDate = {maxDate}")
+
+    obj = dict()
+    for date in daterange(minDate, maxDate):
+        obj[date] = dict()
+
+    print(obj)
+
+    for msg in data:
+        date = datetime.datetime.strptime(msg["date"], "%Y/%m/%d %H:%M:%S").date()
+        author = msg["author"]
+        if author in obj[date]:
+            obj[date][author] += 1
+        else:
+            obj[date][author] = 1 
+    
+    # for msg in data:
+    #     date = datetime.strptime(msg["date"]).date()
+    #     member = msg["author"]
+    #     if member in obj[date][member]:
+    #         obj[date][member] += 1
+    #     else:
+    #         obj[date][member] = 1
+
+    return obj
 
 sns.set_theme(font="IPAexGothic")
 
@@ -31,28 +74,12 @@ filePath = tkinter.filedialog.askopenfilename(filetype=fileType, initialdir=curr
 with open(filePath, "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# for msg in data:
-#     datetxt = msg["date"]
-#     date = pd.to_datetime(datetxt)
-#     msg["date"] = date.strftime("%d")
+distributed_data = distribute_on_timeline(data)
+df = pd.DataFrame.from_dict(data=distributed_data).T
+print(df)
 
-
-df = pd.DataFrame(data=data)
-# df = df.sort_values("date")
-pd.to_datetime(df["date"])
-# df = pd.read_json(filePath)
-# tips = sns.load_dataset(filePath)
-# sns.relplot(
-#     data=tips,
-#     x="total_bill",
-#     y="tip",
-#     col="time",
-#     hue="smoker",
-#     style="smoker",
-#     size="size",
-# )
 dayCount = calendar.monthrange(2025, 6)[1]
-sns.displot(data=df, x="date", hue="author", bins=dayCount, kde=True)
+sns.displot(data=df, hue="author", bins=dayCount, kde=True)
 plt.savefig(get_now_datetime_txt() + ".png")
 plt.show()
 sns.countplot(data=df, x="date", hue="author")
